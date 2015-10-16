afd13f8ed8eab45e9d1223ff9b5413036950faa9
Johan Hidding <johannes.hidding@gmail.com>
2015-10-16 14:35:34 +0200

    added test for parallel execution using time.sleep

--- test/test_runner.py
+++ test/test_runner.py
@@ -36,7 +36,7 @@ def test_runner_02():
     
     assert run(C) == 42
 
-def test_parellel_runner_01():
+def test_parallel_runner_01():
     A = add(1, 1)
     B = sub(3, A)
 
@@ -44,4 +44,21 @@ def test_parellel_runner_01():
     C = sum(bind(*multiples))
     
     assert run_parallel(C, 4) == 42
+
+import time
+from itertools import repeat
+
+@schedule
+def delayed(a, delay=1.0):
+    time.sleep(delay)
+    return a
+
+def test_parallel_runner_02():
+    A = repeat(delayed(1, 0.1), 4)
+    B = sum(bind(*A))
     
+    start = time.time()
+    assert run_parallel(B, 4) == 4
+    end = time.time()
+    assert (end - start) < 0.2 # liberal upper limit for running time
+
