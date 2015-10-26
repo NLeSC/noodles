b1fb266609423ea4be83488a4fedbffe1f3bef66
Johan Hidding <j.hidding@esciencecenter.nl>
2015-10-26 16:03:47 +0100

    added more documentation to datamodel

--- engine/datamodel.py
+++ engine/datamodel.py
@@ -62,6 +62,28 @@ def get_workflow(x):
     return None
 
 def serialize_arguments(bound_args):
+    """
+    Generator that takes the bound_args output of signature().bind and iterates
+    over all the arguments, returning reproducable addresses of each
+    argument. 
+    
+    An address is stored in an `ArgumentAddress` object (a
+    named tuple), containing the kind of argument (regular, variadic or keyword),
+    the name of the argument, and, if not a regular argument, a key.
+    In the case of a variadic argument this is an integer index into the
+    variadic arguments list, in the case of a keyword argument it is a
+    string. For regular arguments the key is set to `None`.
+    
+    @param bound_args:
+        Bound arguments structure, as described in the documentation of the
+        `inspect` module.
+    @type bound_args: BoundArguments
+    
+    @returns:
+        Generates (kind, name, key)-tuples representing an address into the
+        argument structure.
+    @rtype: Iterator[ArgumentAddress]
+    """
     for p in bound_args.signature.parameters.values():
         if p.kind == Parameter.VAR_POSITIONAL:
             for i, v in enumerate(bound_args.arguments[p.name]):
@@ -76,12 +98,20 @@ def serialize_arguments(bound_args):
         yield ArgumentAddress(ArgumentKind.regular, p.name, None)
 
 def ref_argument(bound_args, address):
+    """
+    Taking a bound_args object, and an ArgumentAddress, retrieves the data
+    currently stored in bound_args for this particular address.
+    """
     if address.kind == ArgumentKind.regular:
         return bound_args.arguments[address.name]
             
     return bound_args.arguments[address.name][address.key]
 
 def set_argument(bound_args, address, value):
+    """
+    Taking a bound_args object, and  an ArgumentAddress and a value,
+    sets the value pointed to by the address to `value`.
+    """
     if address.kind == ArgumentKind.regular:
         bound_args.arguments[address.name] = value
         return
@@ -89,12 +119,21 @@ def set_argument(bound_args, address, value):
     bound_args.arguments[address.name][address.key] = value
 
 def format_address(address):
+    """
+    Formats an ArgumentAddress for human reading.
+    """
     if address.kind == ArgumentKind.regular:
         return address.name
         
     return "{0}[{1}]".format(address.name, address.key)
 
 def insert_result(node, address, value):
+    """
+    Runs `set_argument`, but checks first wether the data location is not
+    already filled with some data. In any normal circumstance this checking
+    is redundant, but if we don't give an error here the program would continue
+    with unexpected results.
+    """
     a = ref_argument(node.bound_args, address)
     if a != Parameter.empty:
         raise RuntimeError("Noodle panic. Argument {arg} in " \
@@ -103,7 +142,50 @@ def insert_result(node, address, value):
     
     set_argument(node.bound_args, address, value)
   
-def merge_workflow(f, bound_args):    
+def merge_workflow(f, bound_args):
+    """
+    Takes a function and a set of arguments it needs to run on. Returns a newly
+    constructed workflow representing the promised value from the evaluation of
+    the function with said arguments. 
+    
+    These arguments are stored in a BoundArguments object matching to the 
+    signature of the given function `f`. That is, bound_args was constructed 
+    by doing:
+        > inspect.signature(f).bind(*args, **kwargs)
+    
+    The arguments stored in the `bound_args` object are filtered on being
+    either 'plain', or 'promised'. If an argument is promised, the value
+    it represents is not actually available and needs to be computed by
+    evaluating a workflow.
+    
+    If an argument is a promised value, the workflow representing the value
+    is added to the new workflow. First all the nodes in the original workflow,
+    if not already present in the new workflow from an earlier argument,
+    are copied to the new workflow, and a new entry is made into the link
+    dictionary. Then the links in the old workflow are also added to the
+    link dictionary. Since the link dictionary points from nodes to a _set_
+    of `ArgumentAddress`es, no links are duplicated.
+    
+    In the `bound_args` object the promised value is replaced by the `empty`
+    object, so that we can see which arguments still have to be evaluated.
+    
+    Doing this for all promised value arguments in the bound_args object,
+    results in a new workflow with all the correct dependencies represented
+    as links in the graph.
+    
+    @param bound_args:
+        Object containing the argument bindings for the function that is
+        being called.
+    @type bound_args: BoundArguments
+    
+    @param f:
+        Function being called.
+    @type f: Callable
+    
+    @returns:
+        New workflow.
+    @rtype: Workflow
+    """
     variadic = next((x.name for x in bound_args.signature.parameters.values()
         if x.kind == Parameter.VAR_POSITIONAL), None)
 
