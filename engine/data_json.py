758ef7734c04360a90fd75ab9ce5b6a281596c12
Johan Hidding <j.hidding@esciencecenter.nl>
2015-11-10 23:32:09 +0100

    fix style

--- engine/data_json.py
+++ engine/data_json.py
@@ -13,15 +13,16 @@ def address_to_object(a):
 def node_to_object(node):
     return { 'module':    node.module,
              'name':      node.name,
-             'arguments': [{ 'address': address_to_object(a),
-                             'value':   v } for a, v in node.arguments] }
+             'arguments': [ { 'address' : address_to_object(a),
+                              'value'   : v }
+                              for a, v in node.arguments ] }
 
 def remap_links(remap, links):
     return [ { 'node': remap[source],
-               'to':   [ { 'node':    remap[node],
-                           'address': address_to_object(address) }
-                             for node, address in target ] }
-                for source, target in links.items() ]
+               'to':   [ { 'node'    : remap[node],
+                           'address' : address_to_object(address) }
+                           for node, address in target ] }
+               for source, target in links.items() ]
 
 def remap_nodes(remap, nodes):
     return dict((remap[i], node_to_object(n.node()))
@@ -30,10 +31,10 @@ def remap_nodes(remap, nodes):
 def workflow_to_object(workflow):
     remap = dict(zip(workflow.nodes.keys(), count()))
 
-    return { "root"  : remap[workflow.root],
-             "nodes" : remap_nodes(remap, workflow.nodes),
-             "links" : remap_links(remap, workflow.links) }
+    return { 'root'  : remap[workflow.root],
+             'nodes' : remap_nodes(remap, workflow.nodes),
+             'links' : remap_links(remap, workflow.links) }
 
 def workflow_to_json(workflow):
-    return json.dumps(workflow_to_object(workflow),
+    return json.dumps(workflow_to_oboject(workflow),
                 sort_keys = True, indent = 2)
