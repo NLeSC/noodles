c35501af2c68e48ab6aa35e58381a4331249c24a
Johan Hidding <j.hidding@esciencecenter.nl>
2015-11-10 10:25:03 +0100

    change some comments

--- test/test_runner.py
+++ test/test_runner.py
@@ -19,12 +19,12 @@ def mul(a, b):
 @schedule
 def sum(a, buildin_sum = sum):
     return buildin_sum(a)
-        
+
 def test_runner_01():
     A = value(1)
     B = value(1)
     C = add(A, B)
-    
+
     assert run(C) == 2
 
 def test_runner_02():
@@ -32,8 +32,8 @@ def test_runner_02():
     B = sub(3, A)
 
     multiples = [mul(add(i, B), A) for i in range(6)]
-    C = sum(bind(*multiples))
-    
+    C = sum(gather(*multiples))
+
     assert run(C) == 42
 
 def test_parallel_runner_01():
@@ -41,8 +41,8 @@ def test_parallel_runner_01():
     B = sub(3, A)
 
     multiples = [mul(add(i, B), A) for i in range(6)]
-    C = sum(bind(*multiples))
-    
+    C = sum(gather(*multiples))
+
     assert run_parallel(C, 4) == 42
 
 import time
@@ -56,9 +56,8 @@ def delayed(a, delay=1.0):
 def test_parallel_runner_02():
     A = repeat(delayed(1, 0.1), 4)
     B = sum(bind(*A))
-    
+
     start = time.time()
     assert run_parallel(B, 4) == 4
     end = time.time()
     assert (end - start) < 0.2 # liberal upper limit for running time
-
