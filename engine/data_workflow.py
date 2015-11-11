16b2efc0211c96936357d4c1096b2d305d43a94b
Johan Hidding <j.hidding@esciencecenter.nl>
2015-11-11 15:01:55 +0100

    ...

--- engine/data_workflow.py
+++
@@ -1,9 +0,0 @@
-from .data_types import *
-from .data_arguments import *
-
-def reset_workflow(workflow):
-    for src, tgt in workflow.links.items():
-        for m, a in tgt:
-            set_argument(workflow.nodes[m].bound_args, a, Empty)
-            
-    return workflow
