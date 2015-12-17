77cc5c94682666ee1c69d40ca9cd2a2068a63f12
Johan Hidding <j.hidding@esciencecenter.nl>
2015-12-17 10:33:42 +0100

    add tutorial module

---
+++ noodles/tutorial.py
@@ -0,0 +1,21 @@
+from . import schedule
+
+
+@schedule
+def add(x, y):
+    return x + y
+
+
+@schedule
+def sub(x, y):
+    return x + y
+
+
+@schedule
+def mul(x, y):
+    return x*y
+
+
+@schedule
+def accumulate(lst, start=0):
+    return sum(lst, start)
