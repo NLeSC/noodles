6ead607051ce4a4ae76edaa3495e940b1022e421
Johan Hidding <j.hidding@esciencecenter.nl>
2016-02-19 11:22:08 +0100

    ready for intermediate merge with master

--- test/test_xenon/test_job_submission.py
+++ test/test_xenon/test_job_submission.py
@@ -6,10 +6,11 @@ import uuid
 from .test_docker import (Container)
 
 
-def test_job_submission():
-    session = str(uuid.uuid4())
-    docker_client = docker.Client()
-    with Container(docker_client, session, 0):
-        config = XenonConfig(
-            jobs_scheme='ssh',
-            location='0.0.0.0')
+# def test_job_submission():
+#     session = str(uuid.uuid4())
+#     docker_client = docker.Client()
+#     with Container(docker_client, session, 0):
+#         certificate = xenon.credentials.
+#         config = XenonConfig(
+#             jobs_scheme='ssh',
+#             location='0.0.0.0',)
