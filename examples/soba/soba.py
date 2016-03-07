076c5bf21349a4bb577cb28110cdf4d12a928063
Johan Hidding <j.hidding@esciencecenter.nl>
2016-03-07 18:25:30 +0100

    get better output in Soba

--- examples/soba/soba.py
+++ examples/soba/soba.py
@@ -1,17 +1,15 @@
 import noodles
 from noodles.workflow import (get_workflow)
-from noodles.run.local import (threaded_worker)
-from noodles.run.coroutines import (Connection, coroutine_sink, siphon_source)
-from noodles.run.experimental import (Logger)
+from noodles.run.coroutines import (Connection, coroutine_sink)
+from noodles.run.experimental import (logging_worker)
 from noodles.display import SimpleDisplay
 
-from collections import namedtuple
 import subprocess
 import sys
 import argparse
 import json
-import threading
 import time
+import shlex
 
 
 class Job:
@@ -24,21 +22,36 @@ class Job:
 
 
 def dynamic_exclusion_worker(display, n_threads):
-    result_source, job_sink = threaded_worker(n_threads).setup()
+    """This worker allows mutualy exclusive jobs to start safely. The
+    user provides the information on which jobs exclude the simultaneous
+    execution of other jobs::
+
+        a = task()
+        b = task()
+
+        update_hints(a, {'task': '1', 'exclude': ['2']})
+        update_hints(b, {'task': '2', 'exclude': ['1']})
+
+        run(gather(a, b))
+
+    Using this worker, when task ``a`` is sent to the underlying worker,
+    task ``b`` is blocked until ``a`` completes, and vice versa.
+    """
+    result_source, job_sink = logging_worker(n_threads, display).setup()
 
     jobs = {}
     key_task = {}
 
-    log = Logger()
-    log_job = log.job_sink()
-    log_result = log.result_sink()
-
     @coroutine_sink
     def pass_job():
+        """The scheduler sends jobs to this coroutine. If the 'exclude' key
+        is found in the hints, it is run in exclusive mode. We keep an internal
+        record of these jobs, and whether they are 'waiting', 'running' or 'done'.
+        """
         while True:
             key, job = yield
 
-            if (job.hints and 'exclude' in job.hints):
+            if job.hints and 'exclude' in job.hints:
                 j = Job(task=job.hints['task'],
                         exclude=job.hints['exclude'],
                         state='waiting',
@@ -49,56 +62,57 @@ def dynamic_exclusion_worker(display, n_threads):
                 try_to_start(j.task)
 
             else:
-                log_job.send((key, job))
                 job_sink.send((key, job))
 
     def is_not_running(task):
+        """Checks if a task is not running."""
         return not (task in jobs and jobs[task].state == 'running')
 
     def try_to_start(task):
+        """Try to start a task. This only succeeds if the task hasn't already
+        run, and no jobs are currently running that is excluded by the task."""
         if jobs[task].state != 'waiting':
             return
 
         if all(is_not_running(i) for i in jobs[task].exclude):
             jobs[task].state = 'running'
             key, job = jobs[task].key, jobs[task].job
-            log_job.send((key, job))
             job_sink.send((key, job))
 
     def finish(key):
+        """Finish a job. This function is called when we recieve a result."""
         task = key_task[key]
         jobs[task].state = 'done'
         for i in jobs[task].exclude:
             try_to_start(i)
 
     def pass_result():
+        """Recieve a result; finish the task in the register and send the result
+        back to the scheduler."""
         for key, status, result, err in result_source:
             if key in key_task:
                 finish(key)
 
-            log_result.send((key, status, result, err))
             yield (key, status, result, err)
-
-    t_log = threading.Thread(target=display, args=(log,))
-    t_log.daemon = True
-    t_log.start()
 
     return Connection(pass_result, pass_job)
 
 
 def run(wf, *, display, n_threads=1):
+    """Run the workflow using the dynamic-exclusion worker."""
     worker = dynamic_exclusion_worker(display, n_threads)
     return noodles.Scheduler(error_handler=display.error_handler)\
         .run(worker, get_workflow(wf))
 
 
-@noodles.schedule_hint(display='{task}', confirm=True)
+@noodles.schedule_hint(display='{cmd}', confirm=True)
 def system_command(cmd, task):
-    time.sleep(0.5)
+    cmd_split = shlex.split(cmd)  # list(shlex.shlex(cmd))
     p = subprocess.run(
-        cmd, stderr=subprocess.PIPE, universal_newlines=True)
+        cmd_split, stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE, universal_newlines=True)
     p.check_returncode()
-    return 0
+    return p.stdout
 
 
 def make_job(cmd, task_id, exclude):
@@ -130,7 +144,7 @@ if __name__ == "__main__":
     args = parser.parse_args(sys.argv[1:])
 
     input = json.load(open(args.target, 'r'))
-    jobs = [make_job(['echo', '--> ', str(td['command'].split()), '<--'],
+    jobs = [make_job(td['command'],
                      td['task'], td['exclude']) for td in input]
     wf = noodles.gather(*jobs)
     with SimpleDisplay(error_filter) as display:
