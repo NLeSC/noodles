1d97d05457d4005ca08d18a9959ac9cc06ed427d
Johan Hidding <j.hidding@esciencecenter.nl>
2016-05-10 11:59:01 +0200

    expanded static sum example with dynamic example

--- examples/static_sum.py
+++ examples/static_sum.py
@@ -1,17 +1,36 @@
-from noodles import (run_parallel)
+from noodles import (run_parallel, schedule)
 from noodles.tutorial import (add)
 
-def static_sum(values):
-    if len(values) > 2:
+import numpy as np
+
+
+def static_sum(values, limit_n=1000):
+    """Example of static sum routine."""
+    if len(values) < limit_n:
+        return sum(values)
+
+    else:
         half = len(values) // 2
-        return add(static_sum(values[:half]), static_sum(values[half:]))
+        return add(
+                static_sum(values[:half], limit_n),
+                static_sum(values[half:], limit_n))
 
-    if len(values) == 2:
-        return add(values[0], values[1])
 
-    if len(values) == 1:
-        return values[0]
+@schedule
+def dynamic_sum(values, limit_n=1000, acc=0, depth=4):
+    """Example of dynamic sum."""
+    if len(values) < limit_n:
+        return acc + sum(values)
+    
+    if depth > 0:
+        half  = len(values) // 2
+        return add(
+            dynamic_sum(values[:half], limit_n, acc, depth=depth-1),
+            dynamic_sum(values[half:], limit_n, 0, depth=depth-1))
 
-result = run_parallel(static_sum(range(10)), 4)
+    return dynamic_sum(values[limit_n:], limit_n, acc + sum(values[:limit_n]), depth)
+        
+
+result = run_parallel(dynamic_sum(range(1000000000), 1000000), 4)
 print(result)
 
