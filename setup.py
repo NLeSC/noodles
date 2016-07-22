41038a029dbbe81d954d380eb94952e90b39b206
Johan Hidding <j.hidding@esciencecenter.nl>
2016-07-22 14:38:14 +0200

    fixed example/run-on-das.py, exit is not clean but it works

--- setup.py
+++ setup.py
@@ -12,12 +12,13 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='Noodles',
-    version='0.1.9300',
+    version='0.1.9301',
     description='Workflow Engine',
     author='Johan Hidding',
     url='https://github.com/NLeSC/noodles',
     packages=[
-        'noodles', 'noodles.serial', 'noodles.run', 'noodles.display',
+        'noodles', 'noodles.serial', 'noodles.run', 'noodles.run.xenon',
+        'noodles.display',
         'noodles.interface', 'noodles.workflow', 'noodles.files',
         'noodles.prov'],
 
