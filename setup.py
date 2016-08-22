bf18672b6234476f7b4e71885f5c5efd5e45067d
Johan Hidding <j.hidding@esciencecenter.nl>
2016-08-22 13:56:56 +0200

    version 0.2.0

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='Noodles',
-    version='0.1.9301',
+    version='0.2.0',
     description='Workflow Engine',
     author='Johan Hidding',
     url='https://github.com/NLeSC/noodles',
