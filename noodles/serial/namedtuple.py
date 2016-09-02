7fdf410e6a758e7da52d6aea8d5cd77d9b11f4bb
Johan Hidding <j.hidding@esciencecenter.nl>
2016-09-02 15:37:02 +0200

    add optional automatic namedtuple serialisation, not 100% reliable, tests wether object is tuple and has `_fields` attribute.

--- noodles/serial/namedtuple.py
+++ noodles/serial/namedtuple.py
@@ -1,4 +1,5 @@
-from .registry import (Serialiser)
+from .registry import (Registry, Serialiser)
+from ..utility import (object_name, look_up)
 
 
 class SerNamedTuple(Serialiser):
@@ -10,3 +11,35 @@ class SerNamedTuple(Serialiser):
 
     def decode(self, cls, data):
         return cls(*data)
+
+
+class SerAutoNamedTuple(Serialiser):
+    def __init__(self):
+        super(SerAutoNamedTuple, self).__init__('<namedtuple>')
+
+    def encode(self, obj, make_rec):
+        return make_rec({
+            'name': object_name(type(obj)),
+            'data': tuple(obj)})
+
+    def decode(self, cls, data):
+        return look_up(data['name'])(*data['data'])
+
+
+def is_namedtuple(obj):
+    return isinstance(obj, tuple) and hasattr(obj, '_fields')
+
+
+def namedtuple_hook(obj):
+    if is_namedtuple(obj):
+        return '<namedtuple>'
+    else:
+        return None
+
+
+def registry():
+    return Registry(
+        hooks={
+            '<namedtuple>': SerAutoNamedTuple()
+        },
+        hook_fn=namedtuple_hook)
--- test/test_namedtuple.py
+++ test/test_namedtuple.py
@@ -1,20 +1,30 @@
 from collections import namedtuple
-from noodles.serial.namedtuple import SerNamedTuple
-from noodles.serial import (Registry, base)
+from noodles import serial
+from noodles.serial.namedtuple import (SerNamedTuple)
+from noodles.serial import (Registry)
 
 A = namedtuple('A', ['x', 'y'])
+B = namedtuple('B', ['u', 'v'])
 
 
-def registry():
+def registry1():
     return Registry(
-        parent=base(),
+        parent=serial.base(),
         types={
             A: SerNamedTuple(A)
         })
 
 
+def registry2():
+    return serial.base() + serial.namedtuple()
+
+
 def test_namedtuple_00():
     a = A(73, 38)
-    r = registry()
+    r = registry1()
     assert r.deep_decode(r.deep_encode(a)) == a
 
+    b = B(783, 837)
+    r = registry2()
+    print(r.deep_encode(b))
+    assert r.deep_decode(r.deep_encode(b)) == b
