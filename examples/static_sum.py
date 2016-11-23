ae272e110bda95329f38c1b802567fa481596239
Johan Hidding <j.hidding@esciencecenter.nl>
2016-11-23 17:27:41 +0100

    fix style errors in examples

--- examples/static_sum.py
+++ examples/static_sum.py
@@ -1,7 +1,7 @@
 from noodles import (run_parallel, schedule)
 from noodles.tutorial import (add)
 
-import numpy as np
+# import numpy as np
 
 
 def static_sum(values, limit_n=1000):
@@ -21,16 +21,16 @@ def dynamic_sum(values, limit_n=1000, acc=0, depth=4):
     """Example of dynamic sum."""
     if len(values) < limit_n:
         return acc + sum(values)
-    
+
     if depth > 0:
-        half  = len(values) // 2
+        half = len(values) // 2
         return add(
             dynamic_sum(values[:half], limit_n, acc, depth=depth-1),
             dynamic_sum(values[half:], limit_n, 0, depth=depth-1))
 
-    return dynamic_sum(values[limit_n:], limit_n, acc + sum(values[:limit_n]), depth)
-        
+    return dynamic_sum(values[limit_n:], limit_n,
+                       acc + sum(values[:limit_n]), depth)
+
 
 result = run_parallel(dynamic_sum(range(1000000000), 1000000), 4)
 print(result)
-
