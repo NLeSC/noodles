84c276ff9ee9473df13dbc96538419e7854386de
Johan Hidding <johannes.hidding@gmail.com>
2017-09-05 11:18:39 +0200

    set pickle protocol to 4

--- noodles/serial/pickle.py
+++ noodles/serial/pickle.py
@@ -9,11 +9,13 @@ class PickleString(Serialiser):
         super(PickleString, self).__init__(cls)
 
     def encode(self, obj, make_rec):
-        data = base64.b64encode(pickle.dumps(obj)).decode('ascii')
+        data = base64.b64encode(pickle.dumps(obj, protocol=4)).decode('ascii')
         return make_rec(data)
 
     def decode(self, cls, data):
-        return pickle.loads(base64.b64decode(data.encode('ascii')))
+        return pickle.loads(
+            base64.b64decode(data.encode('ascii')),
+            protocol=4)
 
 
 def registry():
