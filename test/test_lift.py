bd5862319ed824c8e03bb73f8ef4ccc8c099da62
Johan Hidding <johannes.hidding@gmail.com>
2017-09-18 15:33:27 +0200

    lifting test

--- test/test_lift.py
+++ test/test_lift.py
@@ -1,5 +1,19 @@
 from noodles import (schedule, run_single, gather, lift)
 from noodles.tutorial import (add, sub, mul)
+from collections import OrderedDict
+
+
+@schedule
+def g(x):
+    return x['a'] + x['b']
+
+def test_lift_ordered_dict():
+    x = OrderedDict()
+    x['a'] = 1
+    x['b'] = add(1, 2)
+    y = g(lift(x))
+    result = run_single(y)
+    assert result == 4
 
 
 class A:
