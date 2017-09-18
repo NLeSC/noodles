66fce7b4fea5a06418aa304a88cc9732b1aad586
Vincent van Hees <vincentvanhees@gmail.com>
2017-09-18 16:36:29 +0200

    nbsphinx added to conf.py extensions and notebook added to toc in index.rst

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -46,6 +46,7 @@ extensions = [
     'sphinx.ext.todo',
     'sphinx.ext.coverage',
     'sphinx.ext.mathjax',
+    'nbsphinx',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
