55f8d0c680ff835985495dada11da43d32144542
Johan Hidding <johannes.hidding@gmail.com>
2017-09-25 22:56:11 +0200

    export get_workflow function and use it in workflow plotting routine

--- noodles/tutorial.py
+++ noodles/tutorial.py
@@ -1,21 +1,25 @@
 from inspect import Parameter
 from graphviz import Digraph
 
-from . import schedule, schedule_hint
+from . import schedule, schedule_hint, get_workflow
 
-#scheduled functions
+
+# scheduled functions
 
 @schedule
 def add(x, y):
     return x + y
 
+
 @schedule_hint(display="{a} + {b}", confirm=True)
 def log_add(a, b):
     return a + b
 
+
 @schedule
 def sub(x, y):
     return x - y
+
 
 @schedule
 def mul(x, y):
@@ -26,7 +30,8 @@ def mul(x, y):
 def accumulate(lst, start=0):
     return sum(lst, start)
 
-#functions for printing workflow graphs in notebooks
+
+# functions for printing workflow graphs in notebooks
 
 def _format_arg_list(a, v):
     if len(a) == 0:
@@ -48,12 +53,18 @@ def _format_arg_list(a, v):
     s += ")"
     return s
 
-def get_workflow_graph(workflow):
+
+def get_workflow_graph(promise):
+    workflow = get_workflow(promise)
+
     dot = Digraph()
     for i,n in workflow.nodes.items():
-        dot.node(str(i), label="{0} \n {1}".format(n.foo.__name__,
-                                                   _format_arg_list(n.bound_args.args, None)))
+        dot.node(str(i), label="{0} \n {1}".format(
+            n.foo.__name__,
+            _format_arg_list(n.bound_args.args, None)))
+
     for i in workflow.links:
         for j in workflow.links[i]:
             dot.edge(str(i), str(j[0]), label=str(j[1].name))
+
     return dot
