2c319fa845a77eeda35455fd2da5789fbafa3826
Johan Hidding <j.hidding@esciencecenter.nl>
2019-06-18 11:53:06 +0200

    fix flake8 errors

--- test/workflows/operators.py
+++ test/workflows/operators.py
@@ -1,11 +1,13 @@
 import noodles
-import numpy as np
+# import numpy as np
 
 from .workflow_factory import workflow_factory
+
 
 @noodles.schedule
 def promise(x):
     return x
+
 
 @workflow_factory(result=42, python_version=(3, 7))
 def answer():
